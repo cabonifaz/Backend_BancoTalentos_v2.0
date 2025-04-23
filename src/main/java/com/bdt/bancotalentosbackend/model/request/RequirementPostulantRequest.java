43efdd6c09a7f43fb03295d1ac13a7d257bf179b
Denis I. A. <dial1901110514@gmail.com>
2025-04-23 16:23:14 -0500

    OBS-FTBO-0018 Generación de enlace con idRq y idPerfil

--- src/main/java/com/bdt/bancotalentosbackend/model/request/RequirementPostulantRequest.java
+++ src/main/java/com/bdt/bancotalentosbackend/model/request/RequirementPostulantRequest.java
@@ -3,10 +3,9 @@ package com.bdt.bancotalentosbackend.model.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
-public class LinkTokenRequest {
-    private List<RequirementPostulantRequest> lstRequerimientos;
+public class RequirementPostulantRequest {
+    private int idRQ;
+    private int idPerfil;
 }
