c8829b2a80d49768d13ffb004d1a0e73bf536574
SuruiDC <soymuyaltoowo@gmail.com>
2026-06-15 17:20:26 -0500

    Modulo de Contactos

--- src/main/java/com/bdt/bancotalentosbackend/service/IUserService.java
+++ src/main/java/com/bdt/bancotalentosbackend/service/IUserService.java
@@ -1,10 +1,14 @@
 package com.bdt.bancotalentosbackend.service;
 
 import com.bdt.bancotalentosbackend.model.request.FavCollectionRequest;
+import com.bdt.bancotalentosbackend.model.request.UpdateUserRequest;
 import com.bdt.bancotalentosbackend.model.response.BaseResponse;
 import com.bdt.bancotalentosbackend.model.response.UserFavListResponse;
+import com.bdt.bancotalentosbackend.model.response.UserInfoResponse;
 
 public interface IUserService {
     BaseResponse addFavouriteCollection (String token, FavCollectionRequest favCollectionRequest);
+    UserInfoResponse getUserInfo(String token);
+    BaseResponse updateUserInfo(String token, UpdateUserRequest updateUserRequest);
     UserFavListResponse getFavourites (String token);
 }
--- src/main/java/com/bdt/bancotalentosbackend/util/Constante.java
+++ src/main/java/com/bdt/bancotalentosbackend/util/Constante.java
@@ -4,6 +4,7 @@ public class Constante {
     // FUNCIONALIDADES
     public static final String ACTUALIZAR_TALENTO = "12";
     public static final String LISTAR_TALENTOS = "13";
+    public static final String ACTUALIZAR_USUARIO = "1040";
 
     public static final String TXT_SEPARADOR = "=========================================";
 
